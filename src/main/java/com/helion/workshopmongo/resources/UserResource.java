c33d4cb1fdc9ae3e4d7a4b028e535237c9c5fb7c
helionporto <helionporto@hotmail.com>
2021-04-04 09:40:15 -0400

    Insert User

--- src/main/java/com/helion/workshopmongo/resources/UserResource.java
+++ src/main/java/com/helion/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.helion.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.helion.workshopmongo.domain.User;
 import com.helion.workshopmongo.dto.UserDTO;
@@ -20,19 +24,29 @@ import com.helion.workshopmongo.services.UserService;
 public class UserResource {
 	
 	@Autowired
-	private UserService userService;
+	private UserService service;
 	
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll(){
-		List<User> list = userService.findAll();
+		List<User> list = service.findAll();
 		List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
 	
 	@GetMapping(value ="/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
-		User user = userService.findById(id);
+		User user = service.findById(id);
 		return ResponseEntity.ok().body( new UserDTO(user));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
 	}
 
 }
