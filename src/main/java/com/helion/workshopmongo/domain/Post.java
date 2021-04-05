3811a1216e4ae62b523562f241d10e1fed413f55
helionporto <helionporto@hotmail.com>
2021-04-05 09:19:45 -0400

    Adding comments to posts

--- src/main/java/com/helion/workshopmongo/domain/Post.java
+++ src/main/java/com/helion/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.helion.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.helion.workshopmongo.dto.AuthorDTO;
+import com.helion.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -17,6 +20,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	
 	public Post() {}
@@ -79,6 +84,11 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
 
 
 	@Override
@@ -107,11 +117,5 @@ public class Post implements Serializable {
 		return true;
 	}
 
-
-	
-	
-	
-	
-	
 	
 }
