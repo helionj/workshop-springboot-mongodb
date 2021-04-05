4010db6008a0ea6a7cf116076f8383372d341d7e
helionporto <helionporto@hotmail.com>
2021-04-05 13:22:08 -0400

    Simple query with @Query

--- src/main/java/com/helion/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/helion/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.helion.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.helion.workshopmongo.domain.Post;
 
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{'title': { $regex: ?0, $options: 'i' } }")
+	public List<Post>findByTitle(String text);
 	
 	public List<Post> findByTitleContainingIgnoreCase(String text);
 
